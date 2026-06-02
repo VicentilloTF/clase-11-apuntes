25cf6a51714cfc1cf86d6c306d5768a7c8e98205
Vicente Tapia <vicente.tapia.f@ug.uchile.cl>
2026-06-02 16:55:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 
-     
-     new Chart(document. getElementByID ("GraficoPie") 
-          const data = {
-  labels: ["Región Metropolitana","Valparaíso",'Biobío', 'La Araucanía','Los Ríos','Maule', 'Antofagasta',  'Ñuble', 'Coquimbo', 'Arica y Parinacota'],
+const data = {
+  labels: [
+    'Región Metropolitana', 'Valparaíso', 'Biobío', 'La Araucanía',
+    'Los Ríos', 'Maule', 'Antofagasta', 'Ñuble', 'Coquimbo', 'Arica y Parinacota'
+  ],
   datasets: [{
     label: 'Matrículas anuales',
     data: [3563, 520, 358, 238, 135, 135, 90, 45, 40, 0],
     backgroundColor: [
-      'rgb(29, 158, 117)',  // Metropolitana — verde
+      'rgb(29, 158, 117)',
       'rgb(24, 95, 165)',
       'rgb(24, 95, 165)',
       'rgb(24, 95, 165)',
@@ -18,16 +19,19 @@
       'rgb(24, 95, 165)',
       'rgb(24, 95, 165)'
     ],
-       borderColor: [#fefefe", "#fefefe"]
-       borderWidth: 2,
+    borderColor: "#fefefe",
+    borderWidth: 2
   }]
 };
-options: {
-     scales:{
-          y:{
-               beginatZero:true
-          }
-     }
-}
-}};
 
+new Chart(document.getElementById('GraficoPie'), {
+  type: 'pie',
+  data: data,
+  options: {
+    plugins: {
+      legend: {
+        position: 'bottom'
+      }
+    }
+  }
+});
